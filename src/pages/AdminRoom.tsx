b709f1b6e4782b81f4a50b6bcd291858f543c91c
FabioSntos <57735438+FabioSntos@users.noreply.github.com>
2021-06-28 12:19:51 -0300

    removing questions

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,16 +1,18 @@
-import { Toaster } from 'react-hot-toast';
+import toast, { Toaster } from 'react-hot-toast';
 
 import { useParams } from 'react-router-dom';
 import { useRoom } from '../hooks/useRoom';
 // import { useAuth } from '../hooks/useAuth';
 
 import logoImage from '../assets/images/logo.svg';
+import deleteImg from '../assets/images/delete.svg';
 
 import Button from '../components/Button';
 import RoomCode from '../components/RoomCode';
 import Question from '../components/Question';
 
 import '../styles/room.scss';
+import { database } from '../services/firebase';
 
 type RoomParams = {
   id: string;
@@ -20,10 +22,22 @@ const AdminRoom = () => {
   <Toaster position="top-center" reverseOrder={false} />;
   // const { user } = useAuth();
   const params = useParams<RoomParams>();
-
   const roomId = params.id;
 
   const { title, questions } = useRoom(roomId);
+
+  async function handleEndRoom() {}
+
+  async function handleDeleteQuestion(questionId: string) {
+    function notify() {
+      toast.success('Pergunta removida com sucesso!');
+    }
+
+    if (window.confirm('Tem certeza que você deseja excluir esta pergunta?')) {
+      await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+      notify();
+    }
+  }
 
   return (
     <div id="page-room">
@@ -50,7 +64,14 @@ const AdminRoom = () => {
                 key={question.id}
                 content={question.content}
                 author={question.author}
-              />
+              >
+                <button
+                  type="button"
+                  onClick={() => handleDeleteQuestion(question.id)}
+                >
+                  <img src={deleteImg} alt="deletar pergunta" />
+                </button>
+              </Question>
             );
           })}
         </div>
